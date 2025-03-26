eee55a355fee55b3b56551b69215c82283c8823e
lucas Francelino de pontes <lucas.francelino@academico.ifpb.edu.br>
2025-03-25 21:05:19 -0300

    cadastro feito com sucesso

---
+++ projeto/JS/cds.js
@@ -0,0 +1,34 @@
+document.addEventListener(
+    'DOMContentLoaded',() =>{
+        const lf = document.getElementById('formLogin')
+
+        if (lf) {
+            lf.addEventListener('submit',async(e) =>{
+                e.preventDefault();
+
+                const email = document.getElementById('email').value
+                const senha = document.getElementById('senha').value
+
+                try {
+                    console.log(JSON.stringify({email,senha}))
+                    const response = await fetch('http://127.0.0.1:3001/api/auth/cru',{ //como o front ta rodando no live server porta 5500 (padrão) e o back na 3001 estava dando erro ao acessar a rota
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        body:JSON.stringify({email,senha}),
+                    });
+
+                    if (response.ok) {
+                        window.location.href = "./index.html"
+                    }
+                    else{
+                        console.error('error ao fazer login nego')
+                    }
+                } catch (er) {
+                    console.error('error ao fazer login negão',er)
+                }
+            })
+        }
+    }
+)
