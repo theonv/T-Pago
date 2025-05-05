75715712f855d2a4a22e28ad32d45404aca8cc89
FabricioEndreon <fabricio.endreon@academico.ifpb.edu.br>
2025-05-05 13:43:24 +0000

    estilização e correção de bugs

--- tp/src/app/home/page.jsx
+++ tp/src/app/home/page.jsx
@@ -23,7 +23,7 @@ export default function Home() {
             <header>
                 <div className="cliente">{nome}</div>
                 <picture>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"></circle><circle cx="12" cy="10" r="3"></circle><path d="M7 20.662V19a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v1.662"></path></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24" fill="none" stroke="#000000" strokeWidth=" " stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"></circle><circle cx="12" cy="10" r="3"></circle><path d="M7 20.662V19a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v1.662"></path></svg>
                 </picture>
             </header>
             <div className="corpo">
@@ -39,7 +39,7 @@ export default function Home() {
                         </div>
                     ))}
                     <div class="container">
-                        <input
+                        <textarea
                             id="put"
                             type="text"
                             value={inputValue}
