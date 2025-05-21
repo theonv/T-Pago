341aab3caf957828743d8390a60e0fd5e0b164bc
Lucas Francelino <lucas.francelino@academico.ifpb.edu.br>
2025-05-21 13:26:52 +0000

    create server

---
+++ back-end/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const morgan = require('morgan');
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+    res.send('Servidor Express rodando!');
+});
+
+app.listen(process.env.SERVER_PORT, () => {
+    console.log(`Servidor rodando na porta ${process.env.SERVER_PORT}`);
+});
