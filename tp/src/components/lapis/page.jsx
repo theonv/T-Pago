08097ba3fcc2d7535055abca2df66618393eb55b
RenanRV3 <renan.vital@academico.ifpb.edu.br>
2025-07-01 16:10:57 +0000

    adição de lapis parte 1

--- tp/src/components/lapis/page.jsx
+++ tp/src/components/lapis/page.jsx
@@ -1,34 +1,8 @@
-import React from "react";
+import React from 'react';
 
 const LapisBranco = () => (
-  <div style={{ position: "absolute", width: "100%", height: "110px", reight: "1%"}}>
-    {/* Conteúdo aqui, se quiser */}
-    
-    <div style={{
-      position: "absolute",
-      right: "30%", // margem da borda direita
-      top: "50%",
-      transform: "translateY(-50%)",
-      display: "flex",
-      /*alignItems: "center",*/
-      /*justifyContent: "center",*/
-      height: "32px",
-      width: "32px",
-      cursor: "pointer"
-    }}>
-      {/* Ícone de lápis branco em SVG */}
-      <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
-        <path
-          d="M3 17.25V21h3.75l11.06-11.06-3.75-3.75L3 17.25z"
-          fill="#fff"
-        />
-        <path
-          d="M20.71 7.04a1.003 1.003 0 0 0 0-1.42l-2.34-2.34a1.003 1.003 0 0 0-1.42 0l-1.83 1.83 3.75 3.75 1.84-1.82z"
-          fill="#fff"
-        />
-      </svg>
-    </div>
-  </div>
-);
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="White" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
+  <path d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+</svg>);
 
 export default LapisBranco;
