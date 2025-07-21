041b0a4baaf85c72e6cf24c1213320ca5d8aed98
Lucas Francelino <lucas.francelino@academico.ifpb.edu.br>
2025-07-21 12:06:40 +0000

    dark theme in header

--- tp/src/components/header/page.jsx
+++ tp/src/components/header/page.jsx
@@ -14,7 +14,7 @@ export default function Header() {
                 height="50"
                 viewBox="0 0 24 24"
                 fill="none"
-                stroke="#000000"
+                stroke={"var(--foreground)"}
                 strokeWidth="2"
                 strokeLinecap="round"
                 strokeLinejoin="round"
